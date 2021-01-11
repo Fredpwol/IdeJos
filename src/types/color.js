2a502938f897956647ed72688b659b53bfa57ca4
Fredpwol <cyborg290@gmail.com>
2021-01-11 14:59:24 +0100

    updated orange theme

--- src/types/color.js
+++ src/types/color.js
@@ -1,6 +1,6 @@
 
 export const greenTheme = "#64dd17";
-export const orangeTheme = "#f15b26";
+export const orangeTheme = "#ff6d00";
 export const dividerColor = '#e4e8ed';
 export const greyOrangeTheme = "#fcba8b";
 export const defaultUri ='https://firebasestorage.googleapis.com/v0/b/idejos-ed640.appspot.com/o/profilePictures%2Fdefaults%2FblankImage.png?alt=media&token=1f80401a-d7ab-4f47-8bfc-323f93127052';
