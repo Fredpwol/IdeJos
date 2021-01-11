c551ff90228a0fac9aec2bb6943304ffc1678506
Fredpwol <cyborg290@gmail.com>
2021-01-11 15:00:45 +0100

    updated ui

--- src/screens/main/Chats.js
+++ src/screens/main/Chats.js
@@ -10,118 +10,44 @@ import Avatar from '../../components/Avatar';
 import {FAB} from 'react-native-paper';
 import {greenTheme, orangeTheme} from '../../types/color';
 import firestore from '@react-native-firebase/firestore';
+import {BadgeContext} from '../../BadgeCounterProvider';
+import {userDataContext} from '../../UserDataProvider';
 import {trim} from '../../types/utils';
+import ChatItem from '../../components/ChatItem';
+
+
 
 const Chats = ({navigation, route}) => {
   const {user} = useContext(AuthContext);
+  const {contactsDetails, chats, userInfo, setUserInfo} = useContext(userDataContext);
   const menuRef = useRef();
-  const [chats, setChats] = useState([]);
   const [content, setContent] = useState(null);
-  const [userInfo, setuserInfo] = useState({});
-  const [status, setStatus] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = firestore()
-      .collection('users')
-      .doc(user.uid)
-      .collection('CHATS')
-      .orderBy('latestMessage.createdAt', 'desc')
-      .onSnapshot(async (querySnapshot) => {
-        const threads = querySnapshot.docs.map((documentSnapshot) => {
-          var data = {
-            _id: documentSnapshot.id,
-            name: '',
-            system: false,
-            latestMessage: {
-              text: '',
-              createdAt: new Date().getTime(),
-              time: new Date().toDateString(),
-              unread: 0,
-            },
-            ...documentSnapshot.data(),
-          };
-          console.log(data.latestMessage);
-          data.name = trim(data.name, 25);
-          let pes = '';
-          if (data.latestMessage?.user) {
-            pes =
-              data.latestMessage.user._id === user.uid
-                ? 'You: '
-                : `${data.latestMessage.user.name}: `;
-          }
-          let sentIcon = null;
-          if (data.latestMessage.sent) {
-            sentIcon = <Icon name="checkmark" color="black" size={20} />;
-            setStatus(sentIcon);
-          }
-          if (data.latestMessage.recieved) {
-            sentIcon = <Icon name="checkmark-done" color="black" />;
-            setStatus(sentIcon);
-          }
-          setStatus(sentIcon);
-          // data.latestMessage.text
-          if (data.latestMessage.text.length > 25) {
-            let msg = pes + data.latestMessage.text;
-            data.latestMessage.text =
-              msg.slice(0, 26).replace(/\n/g, ' ') + '...';
-          } else {
-            data.latestMessage.text =
-              pes + data.latestMessage.text.replace(/\n/g, ' ');
-          }
-          const padZero = (minute) => '0' + minute.toString();
-          let today = new Date();
-          let createdAt = new Date(data.latestMessage.createdAt);
-          if (createdAt.getUTCDate() === today.getUTCDate()) {
-            let hour = (createdAt.getUTCHours() + 1) % 24;
-            let minute = createdAt.getUTCMinutes();
-            if (minute < 10) {
-              minute = padZero(minute);
-            } else {
-              minute = minute.toString();
-            }
-            data.latestMessage.time = hour.toString() + ':' + minute;
-          } else if (createdAt.getUTCDate() == today.getUTCDate() - 1) {
-            data.latestMessage.time = 'Yesterday';
-          } else {
-            data.latestMessage.time = createdAt.toDateString();
-          }
-          return data;
-        });
-        if (threads.length === 0) {
-          setContent(
-            <View
-              style={{alignSelf: 'center', justifyContent: 'center', flex: 3}}>
-              <Text style={{color: 'gray'}}>
-                Recent chats will appear here!
-              </Text>
-            </View>,
-          );
-        } else {
-          setContent(null);
-        }
-        setChats(threads);
+    chats.forEach(data => {
+      setUserInfo((prevInfo) => {
+        let currData = {...prevInfo};
+  
+        currData[data._id] = contactsDetails.filter(
+          (item) => data._id == item._id,
+        )[0];
+        return currData;
       });
-    const unsubscribeUserInfo = firestore()
-      .collection('users')
-      .doc(user.uid)
-      .collection('CHATS')
-      .onSnapshot((snap) => {
-        snap.forEach(async (doc) => {
-          var id = doc.id;
-          const usersnap = await firestore()
-            .collection('users')
-            .doc(doc.id)
-            .get();
-          const {displayName, photoURL} = usersnap.data();
-          setuserInfo((prevInfo) => {
-            let data = {...prevInfo};
-            data[id] = {_id: id, displayName, photoURL};
-            return data;
-          });
-        });
-      });
-    return () => [unsubscribe(), unsubscribeUserInfo()];
-  }, []);
+    })
+    
+    if (chats.length === 0) {
+      setContent(
+        <View
+          style={{alignSelf: 'center', justifyContent: 'center', flex: 3}}>
+          <Text style={{color: 'gray'}}>
+            Recent chats will appear here!
+          </Text>
+        </View>,
+      );
+    } else {
+      setContent(null);
+    }
+  }, [chats, contactsDetails]);
 
   return (
     <Container>
@@ -157,50 +83,14 @@ const Chats = ({navigation, route}) => {
         }
       />
 
+
       {content}
       <FlatList
         scrollToOverflowEnabled
         data={chats}
         keyExtractor={(item) => item._id}
-        renderItem={({item}) => {
-          console.log('info', userInfo);
-
-          return (
-            <ListItem
-              avatar
-              onPress={() =>
-                navigation.navigate('userChat', {contact: userInfo[item._id]})
-              }>
-              <Left>
-                <Avatar source={{uri: userInfo[item._id]?.photoURL}} />
-              </Left>
-              <Body>
-                <Text>{userInfo[item._id]?.displayName}</Text>
-                <Text note>
-                  <Text note style={{color: orangeTheme}}>
-                    {item.latestMessage.text.slice(
-                      0,
-                      item.latestMessage.text.indexOf(':'),
-                    )}
-                  </Text>
-                  {item.latestMessage.text.slice(
-                    item.latestMessage.text.indexOf(':'),
-                  )}
-                </Text>
-              </Body>
-              <Right>
-                <View style={{flexDirection: 'row'}}>
-                  {status}
-                  <Text note>{item.latestMessage.time}</Text>
-                </View>
-                {item.latestMessage.unread !== 0 &&
-                item.latestMessage.unread !== undefined ? (
-                  <Badge status="error" value={item.latestMessage.unread} />
-                ) : null}
-              </Right>
-            </ListItem>
-          );
-        }}
+        renderItem={({item}) => (<ChatItem item={item} userInfo={userInfo} onPress={()=> navigation.navigate('userChat', {contact: userInfo[item._id]})
+        } />) }
       />
       <FAB
         icon="account"
