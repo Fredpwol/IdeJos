1e4ee34e7a89268a4276901fa3404cf908159406
Fredpwol <cyborg290@gmail.com>
2021-01-11 14:57:28 +0100

    added online status icon

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -13,67 +13,75 @@ import {View, TouchableNativeFeedback} from 'react-native';
 import Menu, {MenuItem} from 'react-native-material-menu';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Avatar from './Avatar';
-import {Divider} from 'react-native-elements';
+import {Divider, Badge} from 'react-native-elements';
 import {greenTheme} from '../types/color';
 import {Dimensions} from 'react-native';
-
 
 const {width} = Dimensions.get('window');
 const UserListItem = (props) => {
   const {user, isAdmin, onPress} = props;
   const menuRef = useRef();
   return (
-      <>
-        <ListItem avatar noBorder onPress={onPress}>
-          <Left>
+    <>
+      <ListItem avatar noBorder onPress={onPress}>
+        <Left>
+          <View>
             <Avatar source={{uri: user.photoURL}} />
-          </Left>
-          <Body>
-            <Text>{user.displayName}</Text>
-          </Body>
-          <Right>
-            {user.isAdmin ? (
-              <Text style={{color: greenTheme}}>Admin</Text>
-            ) : isAdmin ? (
-              <Menu
-                ref={menuRef}
-                button={
-                  <Icon
-                    name={'ellipsis-vertical'}
-                    size={18}
-                    onPress={() => menuRef.current.show()}
-                  />
-                }>
-                <MenuItem
-                  onPress={() => {
-                    menuRef.current.hide();
-                  }}>
-                  Remove User
-                </MenuItem>
-                <MenuItem
-                  onPress={() => {
-                    menuRef.current.hide();
-                  }}>
-                  Block User
-                </MenuItem>
-                <MenuItem
-                  onPress={() => {
-                    menuRef.current.hide();
-                  }}>
-                  Make Admin
-                </MenuItem>
-              </Menu>
+            {user.status == 'Online' ? (
+              <Badge
+                status="success"
+                containerStyle={{left: 35, top: 35, position: 'absolute'}}
+                badgeStyle={{width: 15, height: 15, borderRadius: 7}}
+              />
             ) : null}
-          </Right>
-        </ListItem>
-        <Divider
-          style={{
-            backgroundColor: '#c0c0c0',
-            marginTop: 10,
-            marginLeft: width - (width - 50),
-          }}
-        />
-      </>
+          </View>
+        </Left>
+        <Body>
+          <Text>{user.displayName}</Text>
+        </Body>
+        <Right>
+          {user.isAdmin ? (
+            <Text style={{color: greenTheme}}>Admin</Text>
+          ) : isAdmin ? (
+            <Menu
+              ref={menuRef}
+              button={
+                <Icon
+                  name={'ellipsis-vertical'}
+                  size={18}
+                  onPress={() => menuRef.current.show()}
+                />
+              }>
+              <MenuItem
+                onPress={() => {
+                  menuRef.current.hide();
+                }}>
+                Remove User
+              </MenuItem>
+              <MenuItem
+                onPress={() => {
+                  menuRef.current.hide();
+                }}>
+                Block User
+              </MenuItem>
+              <MenuItem
+                onPress={() => {
+                  menuRef.current.hide();
+                }}>
+                Make Admin
+              </MenuItem>
+            </Menu>
+          ) : null}
+        </Right>
+      </ListItem>
+      <Divider
+        style={{
+          backgroundColor: '#c0c0c0',
+          marginTop: 10,
+          marginLeft: width - (width - 50),
+        }}
+      />
+    </>
   );
 };
 
